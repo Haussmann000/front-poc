76ceee38611cd038854e698cfb92c2149f8d140d
r_takaba <r_takaba@satt.jp>
2022-06-18 14:10:14 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app dark>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
+
+export default {
+  components: {
+    VueSlider
+  },
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+
+
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+
+      <v-card>
+        <v-card-title class="headline">
+          顧客情報登録
+        </v-card-title>
+        
+        <v-card-actions class="btn-wrapper">
+            <input type="file" >
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+  .btn-wrapper, .v-sheet {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .headline {
+    margin: 0 auto;
+  }
+
+</style>
