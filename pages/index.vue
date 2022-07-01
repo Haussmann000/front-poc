3851660d8b5e8404703424aa135fbd5a2a7665bb
r_takaba <r_takaba@satt.jp>
2022-07-02 08:50:57 +0900

    ファイルアップロードcomponent追加

--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,10 @@
 
       <v-card>
         <v-card-title class="headline">
-          顧客情報登録
+          情報登録
         </v-card-title>
         
-        <v-card-actions class="btn-wrapper">
-          <v-btn>
-            <input type="file" multiple>
-          </v-btn>
-          <div
-            class="drop-area"
-            @dragenter="drag = true"
-            @dragover.prevent="drag = true"
-            @drop.prevent="drop"
-            @dragleave="dragleave()"
-            :class="{ enter: drag }"
-          >
-            <p>
-              ファイルをドラッグしてください
-            </p>
-          </div>
-        </v-card-actions>
+        <FileUpload></FileUpload>
       </v-card>
     </v-col>
   </v-row>
@@ -31,30 +15,16 @@
 
 <script>
 import draggable from 'vuedraggable'
+import FileUpload from '../components/FileUpload.vue'
 export default {
-  name: 'Draggable',
-  name: 'IndexPage',
-  data() {
-    return {
-      drag: false,
-      file: []
+    name: "Draggable",
+    name: "IndexPage",
+    data() {
+        return {
+            drag: false,
+            file: []
+        };
     }
-  },
-  methods: {
-    drop(event) {
-      this.drag = true
-      const files = event.dataTransfer.files
-      const reader = new FileReader()
-      reader.onload = event => {
-        this.file = event.target.result
-      }
-      reader.readAsDataURL(files[0])
-    },
-    dragleave() {
-      this.drag = false
-      this.file = []
-    }
-}
 }
 </script>
 
@@ -64,36 +34,12 @@ export default {
     padding: 0;
     font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
   }
-  .drop-area {
-    width: 80%;
-    height: 200px;
-    background-color: rgb(63, 71, 73);
-    color: rgb(215, 247, 255);
-    border-radius: 5px;
-    margin: 15px;
-    padding: 5px;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    border: 3px dashed rgb(24, 141, 173);
-    cursor: pointer;
-  }
-  .drop-area p {
-    align-self: center;
-  }
-  .btn-wrapper, .v-sheet {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-  }
-  .enter.drop-area {
-    border: 2px dashed #1867c0;
-    background: #d4e8ff;
-  }
   .headline {
     font-size: 3rem;
     margin: 0 auto;
+    text-align: center;
+    display: flex;
+    justify-content: center;
   }
 
 </style>
